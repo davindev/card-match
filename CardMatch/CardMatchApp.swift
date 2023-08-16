e9d3db8ebdadef327ddacde8ff376a5b161cf75b
davindev <vinnie.jeong@kidsnote.com>
2023-08-16 15:08:50 +0900

    Initial Commit

---
+++ CardMatch/CardMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMatchApp.swift
+//  CardMatch
+//
+//  Created by vinnie on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct CardMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
