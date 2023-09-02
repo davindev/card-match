226a726757f1b0888f6cb7e063c971df11a29e69
davindev <vinnie.jeong@kidsnote.com>
2023-09-02 18:24:28 +0900

    feat: 메인페이지 UI 수정

--- EmojiMatch/ContentView.swift
+++ EmojiMatch/ContentView.swift
@@ -12,10 +12,44 @@ struct ContentView: View {
     UINavigationBar.setAnimationsEnabled(false)
   }
 
+  private let emojis = ["☁️", "🍋", "🎾", "🩷"]
+  @State private var backgroundEmoji = ""
+
   var body: some View {
     NavigationStack {
-      NavigationLink("START") { GameView() }
-        .navigationBarBackButtonHidden(true)
+      ZStack {
+        ForEach(0..<20, id: \.self) { emoji in
+          Text(backgroundEmoji)
+            .font(.system(size: .random(in: 140...200)))
+            .position(
+              x: .random(in: 0...UIScreen.main.bounds.width),
+              y: .random(in: 0...UIScreen.main.bounds.height)
+            )
+        }
+        .onAppear() {
+          backgroundEmoji = emojis.randomElement() ?? ""
+        }
+
+        VStack {
+          Image("logo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300)
+
+          NavigationLink(destination: GameView()) {
+            Text("시작하기")
+              .frame(width: 130, height: 46)
+              .background(Color(red: 252/255.0, green: 178/255.0, blue: 26/255.0))
+              .cornerRadius(8)
+              .font(.custom("LOTTERIACHAB", size: 24))
+              .foregroundColor(Color(red: 255/255.0, green: 252/255.0, blue: 222/255.0))
+          }
+          .padding(.top, 14)
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(Color(red: 255/255.0, green: 252/255.0, blue: 222/255.0))
+      .navigationBarBackButtonHidden(true)
     }
   }
 }
